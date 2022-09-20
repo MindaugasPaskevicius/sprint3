a101fa076325e6b4c28c2308fca6f8ad5ee4b64c
MindaugasPaskevicius <106962850+MindaugasPaskevicius@users.noreply.github.com>
2022-09-20 17:25:35 +0300

    edit css

--- index.php
+++ index.php
@@ -30,7 +30,9 @@ include 'app/update.php';
                     </span>
                 </div>
             </div>
-            <div class="float-end align-middle"><h4 class="align-middle text-white">PROJECT MANAGER</h4></div>
+            <div class="float-end align-middle">
+                <h4 class="align-middle text-white">PROJECT MANAGER</h4>
+            </div>
         </nav>
     </header>
     <main>
@@ -39,47 +41,39 @@ include 'app/update.php';
                 <form action="<?php ($_SERVER['REQUEST_URI']); ?>" method="post">
                     <?php if ($_GET['path'] == "employees" || $_GET['path'] == "") : ?>
                         <div class="form-group">
-                            <label for="name"><?php print (($update == true) ? ("<strong>Edit Employee</strong>") : ("<strong>Add Employee</strong>")); ?></label>
+                            <label for="name"><?php print(($update == true) ? ("<strong>Edit Employee</strong>") : ("<strong>Add Employee</strong>")); ?></label>
                             <div class="form-group d-flex">
-                                <input type="text" class="form-control" name="name" value="<?php print ($update == true ? $first_en : ""); ?>" placeholder="Enter employee's name">
-
+                                <input type="text" class="form-control" name="name" value="<?php print($update == true ? $first_en : ""); ?>" placeholder="Enter employee's name">
                                 <?php
-
                                 if ($update) {
                                     $query = "SELECT projects.id, projects.name FROM projects";
                                     $res = mysqli_query($conn, $query) or die(mysqli_connect_error($query));
-                                    print ("<select class='ml-5 custom-select' name='select_name'>");
-                                    print ("<option value=''selected disabled>Projects</option>");
+                                    print("<select class='ml-5 custom-select' name='select_name'>");
+                                    print("<option value=''selected disabled>Projects</option>");
                                     while ($row = mysqli_fetch_array($res)) {
                                         print "<option value=" . $row['id'] . ">" . $row['name'] . "</option>";
                                     }
-                                    print ("</select>");
+                                    print("</select>");
                                 }
                                 ?>
                             </div>
-
                         </div>
                     <?php else : ?>
                         <div class="form-group">
-                            <label for="name"><?php print (($update == true) ? ("<strong>Edit Project</strong>") : ("<strong>Add Project</strong>")); ?></label>
-                            <input type="text" class="form-control" value="<?php print ($update == true ? $first_en : ""); ?>" name="name" placeholder="Enter project's name">
+                            <label for="name"><?php print(($update == true) ? ("<strong>Edit Project</strong>") : ("<strong>Add Project</strong>")); ?></label>
+                            <input type="text" class="form-control" value="<?php print($update == true ? $first_en : ""); ?>" name="name" placeholder="Enter project's name">
                         </div>
                     <?php endif; ?>
-
                     <div class="form-group">
-
                         <?php if ($update == true) : ?>
                             <button class="btn btn-light" type="submit" name="update">Update</button>
                         <?php else : ?>
                             <button class="btn btn-light" type="submit" name="save">Add</button>
                         <?php endif; ?>
-
                     </div>
                 </form>
-
             </div>
         </div>
-
         <?php
 
         print('<div class="container mt-5"><table class="table"><thead><tr><th>ID</th><th>' . ($_GET['path'] === 'projects' ?  "Project's Name" : "Employee's Name") . '</th><th>' . ($_GET['path'] === 'projects' ?  "Employee's Name" : "Project's Name") . '</th><th>Action</th>');
@@ -94,8 +88,8 @@ include 'app/update.php';
                     <td>" . $first_en . "</td>
                     <td>" . $second_en . "</td>
                     <td>
-                        <button class='btn-light'><a class='table-button' href=\"?path=" . $table_name . "&delete=$id\">" . ($_GET['path'] === 'projects' ?  "DELETE" : "DELETE") . "</a></button>
-                        <button class='btn-light'><a class='table-button' href=\"?path=" . $table_name . "&edit=$id\">" . ($_GET['path'] === 'projects' ?  "EDIT" : "EDIT") . "</a></button>
+                        <button class='btn-light'><a class='table-button' style='text-decoration:none' href=\"?path=" . $table_name . "&delete=$id\">" . ($_GET['path'] === 'projects' ?  "DELETE" : "DELETE") . "</a></button>
+                        <button class='btn-light'><a class='table-button' style='text-decoration:none' href=\"?path=" . $table_name . "&edit=$id\">" . ($_GET['path'] === 'projects' ?  "EDIT" : "EDIT") . "</a></button>
                     </td>
                 </tr>";
             }
@@ -103,16 +97,16 @@ include 'app/update.php';
             print('<br />');
             print('<br />');
         } else {
-            print ("<tr>
+            print("<tr>
         <td>No data found</td></tr>");
             print('</table>');
         }
         ?>
     </main>
 
-    <footer class="footer mt-auto py-3">
-        <div class="container mr-2 ml-2">
-            <span class="text-white text-center">Kaunas 2022</span>
+    <footer class="footer mt-auto py-3 text-center">
+        <div class="container">
+            <span class="text-white">Kaunas 2022</span>
         </div>
     </footer>
 </body>
